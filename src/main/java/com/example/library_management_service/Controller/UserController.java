c75ef5364054bf4a98ce8b90f1263f5b1821426b
quocthonguth <2251120320@ut.edu.vn>
2025-09-13 11:21:29 +0700

    Update UserController.java

--- src/main/java/com/example/library_management_service/Controller/UserController.java
+++ src/main/java/com/example/library_management_service/Controller/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-@CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS})
+@CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS})
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
